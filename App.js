fe05ed61693d7af53fc929f57ddc407b61ef79d1
Aayush Maheshwari <aayushmaheshwari@Aayushs-MacBook-Pro-2.local>
2023-09-04 20:45:08 +0530

    ignore warnings and console.error on device

--- App.js
+++ App.js
@@ -3,7 +3,9 @@ import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import React from "react";
 import { ToDoScreen } from "./ToDoScreen/ToDoScreen";
 import { AuthScreen } from "./AuthScreen/AuthScreen";
-
+import { LogBox } from "react-native";
+LogBox.ignoreLogs(["Warning: ..."]); // Ignore log notification by message
+LogBox.ignoreAllLogs(); //Ignore all log notifications
 const Stack = createNativeStackNavigator();
 
 export const navigationRef = React.createRef();
